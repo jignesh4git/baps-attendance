80617a5d0ff9c2601c44c93e01e1982ae628d4bc
jignesh4git <dba.jigneshpatel@gmail.com>
2018-01-10 23:41:05 +0530

    fix static

--- baps/settings.py
+++ baps/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,8 +157,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = "/home/static/attendance/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
