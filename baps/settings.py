5618238ed418fc8904fe36a5345d09d63c2aee55
Jignesh Patel <dba.jigneshpatel@gmail.com>
2018-01-11 16:42:43 +0530

    fix db

--- baps/settings.py
+++ baps/settings.py
@@ -99,12 +99,9 @@ WSGI_APPLICATION = 'baps.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-# import dj_database_url
-#
-# DATABASES['default'] = dj_database_url.config()
-# Pick database from environment
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #     'default': {
