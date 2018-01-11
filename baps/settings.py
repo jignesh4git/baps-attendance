0e17fef17a71193f65f365c9a32ce436294b9483
Jignesh Patel <dba.jigneshpatel@gmail.com>
2018-01-11 17:38:53 +0530

    fix db

--- baps/settings.py
+++ baps/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'baps.wsgi.application'
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
 
-DATABASES['default'] = dj_database_url.config()
+
 
 # DATABASES = {
 #     'default': {
@@ -123,6 +123,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
